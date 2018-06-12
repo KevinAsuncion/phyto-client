b8dec0b880638dce221a2d83c330225fc74ceb14
KevinAsuncion <kevinramosasuncion@gmail.com>
2018-06-12 08:32:14 -0700

    updated app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link} from 'react-router-dom'
+// import { BrowserRouter as Router, Route, Link} from 'react-router-dom'
 import './App.css';
 import Navbar from './Navbar';
 import Hero from './Hero';
@@ -9,7 +9,6 @@ import Footer from './Footer';
 export default class App extends Component {
   render() {
     return (
-      <Router>
         <div className="App">
           <header>
             <h1 className="App-title">Phyto</h1>
@@ -21,7 +20,6 @@ export default class App extends Component {
           </main> 
           <Footer/>
         </div>
-      </Router>
     );
   }
 }
