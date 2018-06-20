4218b562e49d5cb91c0f9e53bc0c392d5333272c
KevinAsuncion <kevinramosasuncion@gmail.com>
2018-06-20 09:38:59 -0700

    update local storage

--- src/local-storage.js
+++ src/local-storage.js
@@ -1,15 +1,15 @@
 export const loadAuthToken = () => {
-    return window.localStorage.getItem('authToken');
+    return localStorage.getItem('authToken');
 };
 
 export const saveAuthToken = authToken => {
     try {
-        window.localStorage.setItem('authToken', authToken);
+        localStorage.setItem('authToken', authToken);
     } catch (e) { }
 };
 
 export const clearAuthToken = () => {
     try {
-        window.localStorage.removeItem('authToken');
+        localStorage.removeItem('authToken');
     } catch (e) { }
 };
