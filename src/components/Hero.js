60cd0421c514708acb79ef0a2de875dd6e46356c
KevinAsuncion <kevinramosasuncion@gmail.com>
2018-06-21 14:09:34 -0700

    update hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,7 +7,7 @@ export default class Hero extends Component {
     render() {
         return (
           <div className="hero-container">
-              <h2>Phyto helps you discover and save delicious plant based recipes.</h2>
+              <h2>Discover and save delicious plant based recipes.</h2>
                 <Link to="/signup"><button>Sign up</button></Link> 
                 <p>Already have an account? <Link to="/login"><span id="hero-login-link">Login</span></Link></p>
           </div>
